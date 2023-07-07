bb14bd95e8e24da2e7099e447f7b47f8ba455dd2
frankieag10 <frankieag10@gmail.com>
2023-07-07 09:27:09 -0700

    sprint 10 updates

--- src/components/WeatherCard/WeatherCard.js
+++ src/components/WeatherCard/WeatherCard.js
@@ -1,22 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import { weatherOptions } from "../../utils/constants";
 import { CurrentTemperatureUnitContext } from "../../contexts/CurrentTemperatureUnitContext";
 import "./WeatherCard.css";
 
 function WeatherCard({ day, type, temperature }) {
-  const { currentTemperatureUnit } = React.useContext(CurrentTemperatureUnitContext);
+  const { currentTemperatureUnit } = useContext(CurrentTemperatureUnitContext);
 
-  console.log(temperature?.weather?.temperature?.[currentTemperatureUnit]);
-  // weatherOptions.find((item) => console.log(item.day === day && item.type === type));
-  const weatherOption = weatherOptions.find((item) => {
-    return item.day === day && item.type === type;
-  });
+  const weatherOption = weatherOptions.find((item) => item.day === day && item.type === type);
   const imageSourceUrl = weatherOption?.url || "";
+
+  const temperatureValue = temperature?.weather?.temperature?.[currentTemperatureUnit];
+  const temperatureUnit = currentTemperatureUnit === "F" ? "°F" : "°C";
+
   return (
     <div className="weather-card">
       <span className="weather__temperature">
-        {temperature?.weather?.temperature?.[currentTemperatureUnit]}{" "}
-        {currentTemperatureUnit === "F" ? "°F" : "°C"}
+        {temperatureValue} {temperatureUnit}
       </span>
       <img
         className="weather__bg"
